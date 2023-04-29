27b7e1264facff1530b85b024001350a2e1ddc1f
laireyx <laireyx@gmail.com>
2023-04-29 20:11:04 +0900

    refactor: better use relative path
    With baseUrl set to "./", vscode autocompletes import path as absolute.
    It's a bad practice, see nestjs/typescript-starter#74 for more info.
    Moreover, using absolute path conflicts with module resolution in jest.

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/schema/user.schema';
+import { User, UserSchema } from '../schema/user.schema';
 
 @Module({
   imports: [
