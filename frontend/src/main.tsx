1cd888901759f86116af30b9ef393444d5a29d78
노주찬 <juchan1220@naver.com>
2023-04-29 17:58:07 +0900

    refactor: fix lint error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import App from './App.tsx';
+
 import './index.css';
 import './fonts.css';
 
