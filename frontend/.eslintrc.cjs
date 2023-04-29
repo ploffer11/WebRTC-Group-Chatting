893ed5771fbc0367216f57d2c97ff3a7abe6786e
노주찬 <juchan1220@naver.com>
2023-04-29 17:21:42 +0900

    chore: config eslint plugin import order

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,19 +3,72 @@ module.exports = {
   extends: [
     '../.eslintrc',
     'eslint:recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'react-hooks/exhaustive-deps': [
       'warn', {
         'additionalHooks': '(useRecoilCallback|useRecoilTransaction_UNSTABLE)'
       }
+    ],
+    'import/default': 'off',
+    'import/no-unresolved': 'error',
+    'import/exports-last': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 2,
+        maxEOF: 0
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ["builtin", "external", "internal", ["parent", "sibling", "index"], "object", "type"],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: "{react-dom,react-dom/**,react}",
+            group: "external",
+            position: "before"
+          },
+          {
+            pattern: "@schema/*",
+            group: "internal",
+            position: "before"
+          },
+          {
+            pattern: "*.{css,svg}",
+            patternOptions: { matchBase: true },
+            group: "type",
+            position: "after"
+          },
+        ],
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: { order: 'asc', caseInsensitive: true }
+      }
     ]
   },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      'typescript': {
+        "alwaysTryTypes": true,
+      }
+    },
+
+
+  }
 };
