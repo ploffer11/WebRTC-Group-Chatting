7ed7e33bec4b9e1d378dcdfe43bcf7c45aaec4ad
노주찬 <juchan1220@naver.com>
2023-04-29 16:05:16 +0900

    chore: add eslint config for recoil

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,5 +12,10 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react-hooks/exhaustive-deps': [
+      'warn', {
+        'additionalHooks': '(useRecoilCallback|useRecoilTransaction_UNSTABLE)'
+      }
+    ]
   },
 };
