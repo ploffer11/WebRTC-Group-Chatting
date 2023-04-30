3f7507699c89b08ef680ba4d274a7875bc83e530
노주찬 <juchan1220@naver.com>
2023-04-30 23:05:07 +0900

    refactor: change object routes into element routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import {
   createBrowserRouter,
+  createRoutesFromElements,
   redirect,
+  Route,
   RouterProvider,
 } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
@@ -15,33 +17,35 @@ import Main from './pages/Main/Main.tsx';
 
 import './fonts.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Base />,
-    children: [
-      {
-        index: true,
-        loader: async () => {
-          // TODO: check login state
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path={'/'} element={<Base />}>
+      <Route
+        index
+        loader={async () => {
+          // TODO: if logged in, then redirect to main, else redirect to main
           return redirect('login');
-        },
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      {
-        path: 'main',
-        loader: async () => {
-          // TODO: check login state. if not logged in, then redirect to login page
+        }}
+      />
+      <Route
+        path={'login'}
+        element={<Login />}
+        loader={async () => {
+          // TODO: if logged in, redirect to main
           return null;
-        },
-        element: <Main />,
-      },
-    ],
-  },
-]);
+        }}
+      />
+      <Route
+        path={'main'}
+        element={<Main />}
+        loader={async () => {
+          // TODO: if not logged in, redirect to login
+          return null;
+        }}
+      />
+    </Route>,
+  ),
+);
 
 const queryClient = new QueryClient();
 
