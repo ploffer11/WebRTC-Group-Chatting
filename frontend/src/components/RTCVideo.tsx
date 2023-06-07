282e82951ddc333ae0d0ad5a1fc4754952144dd3
노주찬 <juchan1220@naver.com>
2023-06-08 00:47:11 +0900

    feat: implement starting video chat
    채팅방 진입 이후 video/audio chat을 유저가 원하는 시점에 참가할 수 있도록 연결 방식을 변경했습니다.
    또한 video/audio chat에 사용할 카메라, 마이크를 참가 전에 설정하도록 했습니다.

--- frontend/src/components/RTCVideo.tsx
+++ frontend/src/components/RTCVideo.tsx
@@ -2,45 +2,56 @@ import { MutableRefObject, useEffect, useMemo, useRef } from 'react';
 
 import { Stack } from '@mui/material';
 
-import useChatStore from '../store/chat';
 import useRTCStore from '../store/rtc';
 
 const RTCVideo = () => {
-  const chatStore = useChatStore();
   const rtcStore = useRTCStore();
-  const storeRef = useRef(() => rtcStore);
-
-  useEffect(() => {
-    if (chatStore.socket) storeRef.current().initialize(chatStore.socket);
-  }, [chatStore.socket]);
 
   const streams = useMemo(() => rtcStore.streams ?? {}, [rtcStore.streams]);
+  const streamKeys = Object.keys(rtcStore.streams);
 
   const videoRefs: MutableRefObject<HTMLVideoElement | null>[] = [];
   // eslint-disable-next-line react-hooks/rules-of-hooks
   for (let i = 0; i < 6; i++) videoRefs.push(useRef<HTMLVideoElement>(null));
 
-  const streamKeys = Object.keys(streams);
-
   useEffect(() => {
     streamKeys.forEach((key, idx) => {
       const video = videoRefs[idx].current;
       if (video && video.srcObject !== streams[key]) {
-        console.log('set srcobject');
         video.srcObject = streams[key];
       }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [streamKeys]);
 
+  const myVideoRef = useRef<HTMLVideoElement>(null);
+  const myMediaStream = rtcStore.mediaStream;
+
+  useEffect(() => {
+    if (myMediaStream !== null && myVideoRef.current) {
+      myVideoRef.current.volume = 0; // 하울링 방지
+      myVideoRef.current.srcObject = myMediaStream;
+    }
+  }, [myMediaStream]);
+
   return (
     <Stack direction={'row'} mt={8}>
-      {Object.entries(streams || {}).map((_, idx) => (
+      {myMediaStream !== null && (
         <video
+          style={{ width: '200px', aspectRatio: 16 / 9 }}
+          ref={myVideoRef}
+          onLoadedMetadata={({ target }) => {
+            (target as HTMLVideoElement).play();
+          }}
+        />
+      )}
+
+      {Object.entries(streams).map((_, idx) => (
+        <video
+          style={{ width: '200px', aspectRatio: 16 / 9 }}
           key={idx}
           ref={videoRefs[idx]}
           onLoadedMetadata={({ target }) => {
-            console.log('metadata loaded');
             (target as HTMLVideoElement).play();
           }}
         />
