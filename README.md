2a0c78a51cdf6ffe6675a4076ca4faa8cb4e7e0d
ploffer11 <ploffer11@naver.com>
2023-06-08 23:29:07 +0800

    docs: add images and modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# WebRTC-Group-Chatting
-WebRTC를 통한 Group Chatting 구현
+# WebRTC Group Chatting
+한양대학교의 [김준호](https://github.com/ploffer11/), [박민수](https://github.com/laireyx/), [노주찬](https://github.com/juchan1220) 이 진행한 WebRTC를 사용한 텍스트/음성/화상 채팅 구현 프로젝트입니다. 
+
+# Project Architecture
+![구성도](https://github.com/ploffer11/WebRTC-Group-Chatting/blob/master/images/arch.png)
+
+# Screenshots
+![로그인](https://github.com/ploffer11/WebRTC-Group-Chatting/blob/master/images/login.png)
+
+![텍스트 채팅](https://github.com/ploffer11/WebRTC-Group-Chatting/blob/master/images/textchat.png)
+
+![화상 채팅](https://github.com/ploffer11/WebRTC-Group-Chatting/blob/master/images/videochat.png)
+
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,32 @@
-import { ValidationPipe } from '@nestjs/common';
+import fs from 'node:fs/promises';
+import process from 'node:process';
+
+import { NestApplicationOptions, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const opts: NestApplicationOptions = {};
+
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      opts.httpsOptions = {
+        ca: await fs.readFile('ca.pem'),
+        key: await fs.readFile('private.pem'),
+        cert: await fs.readFile('public.pem'),
+      };
+    }
+  } catch (err) {
+    console.warn('Cannot read HTTPS certificate');
+  }
+
+  const app = await NestFactory.create(AppModule, opts);
+
   app.enableCors({
     origin: true,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
